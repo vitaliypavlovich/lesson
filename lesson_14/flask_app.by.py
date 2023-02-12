46f196f4977af4a6cae836e892c19b4daad9e753
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-02-13 01:10:53 +0300

    Add classwork

--- lesson_14/flask_app.by.py
+++ lesson_14/flask_app.by.py
@@ -1,5 +1,10 @@
 import logging
 
+from lesson_12.models import User, Profile, Address, Purchase, Product
+from sqlalchemy import create_engine
+from sqlalchemy_utils import create_database, database_exists
+from lesson_12 import utils
+
 from flask import Flask, request
 
 logging.basicConfig(level=logging.INFO)
@@ -13,14 +18,19 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         logger.info('Обрабатываю POST запрос')
-        return request.form.to_dict()
+        for key, value in request.form.to_dict().items():
+            logger.info(f'{key}: {value}')
+        return 'hello world'
 
     logger.info('Обрабатываю GET запрос')
-    #return request.args.to_dict()
     for key, value in request.args.to_dict().items():
-        logger.info(f'{key} = {value}')
+        logger.info(f'{key}: {value}')
     return 'hello world'
 
+@app.route("/", methods=["GET", "POST"])
+def abc():
+    if request.methon == 'POST':
+        logger.info('Обрабатываю POST за')
 
 
 if __name__ == "__main__":
