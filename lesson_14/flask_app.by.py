63fdbc5f67c8219985ade68c0c974e662a69a612
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-02-19 01:11:25 +0300

    Add homework

--- lesson_14/flask_app.by.py
+++ lesson_14/flask_app.by.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from lesson_12.utils import create_table
 from lesson_12.models import User, Profile, Address, Purchase, Product
 from lesson_12.services import get_users
-from lesson_12.services import create_user
+from lesson_12 import services
 from flask import Flask, request
 
 logging.basicConfig(level=logging.INFO)
@@ -13,37 +13,60 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+# @app.route("/", methods=["GET"])
+# def user_get():
+#    return get_users(app.session)
 
+# @app.route("/", methods=["POST"])
+# def user_create():
+#     if not request.form.get('email') or not request.form.get('password'):
+#         raise Exception
+#     user = services.create_user(app.session, request.form.get('email'), request.form.get('password'))
+#     return {'user_id': user.id}
 
-# @app.route("/", methods=["GET", "POST"])
-# def index():
-#     if request.method == "POST":
-#         logger.info('Обрабатываю POST запрос')
-#         for key, value in request.form.to_dict().items():
-#             logger.info(f'{key}: {value}')
-#         return 'hello world'
-#
-#     logger.info('Обрабатываю GET запрос')
-#     for key, value in request.args.to_dict().items():
-#         logger.info(f'{key}: {value}')
-#     return 'hello world'
-#
-# @app.route("/", methods=["GET", "POST"])
-# def abc():
-#     if request.methon == 'POST':
-#         logger.info('Обрабатываю POST за')
+# @app.route('/', methods=['POST'])
+# def profile_create():
+#     profile = services.create_profile(app.session, request.form.get('email'), request.form.get('city'),
+#                                       request.form.get('address'), request.form.get('phone'), request.form.get('age'))
+#     return {'profile_id': profile.id}
 
-@app.route("/", methods=["GET"])
-def index():
-   return get_users(app.session)
+# @app.route('/', methods=['POST'])
+# def address_add():
+#     location = services.add_address(app.session, request.form.get('email'), request.form.get('city'),
+#                                       request.form.get('address'))
+#     return {'location_id': location.id}
+
+# @app.route('/', methods=['POST'])
+# def address_update():
+#     location = services.update_address(app.session, request.form.get('email'), request.form.get('old_city'),
+#              request.form.get('old_address'), request.form.get('new_city'), request.form.get('new_address'))
+#     return {'location_id': location.id}
+
+# @app.route("/", methods=["GET"])
+# def user_find():
+#    return services.find_user(app.session)
+
+# @app.route("/", methods=["POST"])
+# def product_create():
+#     product = services.create_product(app.session, request.form.get('name'), request.form.get('price'),
+#                                       request.form.get('count'), request.form.get('comment'))
+#     return {'product_id': product.id}
+
+# @app.route('/', methods=['POST'])
+# def product_update():
+#     product = services.update_product(app.session, request.form.get('product_id'), request.form.get('name'),
+#              request.form.get('price'), request.form.get('count'), request.form.get('comment'))
+#     return {'product_id': product.id}
+
+# @app.route('/', methods=['POST'])
+# def product_delete():
+#     product = services.delete_product(app.session, request.form.get('product_id'))
 
 @app.route("/", methods=["POST"])
-def post_user():
-    if not request.form.get('email') or not request.form.get('password'):
-        raise Exception
-    user = create_user(app.session, request.form.get('email'), request.form.get('password'))
-    return {'user_id': user.id}
-
+def purchase_create():
+    purchase = services.buy_product(app.session, request.form.get('email'), request.form.get('product'),
+                                      request.form.get('count'))
+    return {'purchase_id': purchase.id}
 
 if __name__ == "__main__":
     engine = create_engine("postgresql://aloa:aloa@localhost/aloa")
